bc558104d1cd4d0787b31b146e6876bb57d1ec79
黑色的白兔子 <0500aa@protonmail.com>
2021-01-12 00:35:15 +0800

    修改了一个将date打成data导致的无法序列化的错误

--- redis/src/main/java/pers/bwr/learn/redis/controller/StudentHandler.java
+++ redis/src/main/java/pers/bwr/learn/redis/controller/StudentHandler.java
@@ -2,10 +2,13 @@ package pers.bwr.learn.redis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pers.bwr.learn.redis.entity.Student;
+
+import javax.swing.text.AbstractDocument;
 
 @RestController
 public class StudentHandler {
--- redis/src/main/java/pers/bwr/learn/redis/entity/Student.java
+++ redis/src/main/java/pers/bwr/learn/redis/entity/Student.java
@@ -1,13 +1,16 @@
 package pers.bwr.learn.redis.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 public class Student implements Serializable {
     private Integer id;
     private String name;
     private Double score;
-    private Data birthday;
+    private Date birthday;
 }
